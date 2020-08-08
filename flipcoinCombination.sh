f40d49ed773f3329e6034cfc66a5cdaecf701d35
vivek231098 <menonvivek196@gmail.com>
2020-08-08 17:43:04 +0530

    Flipped a coin

--- flipcoinCombination.sh
+++ flipcoinCombination.sh
@@ -1,1 +1,17 @@
-
+Heads=0
+Tails=0
+i=0
+declare -A dict
+while [[ $Heads -lt 50 || $Tails -lt 50 ]]
+do
+   randomCheck=$((RANDOM%2))
+   if [ $randomCheck -eq 1 ]
+   then
+      ((Heads++))
+   dict[ "$i" ]="Heads"
+   else
+      ((Tails++))
+      dict[ "$i" ]="Tails"
+   fi
+((i++))
+done
